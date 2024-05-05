2ee25c6633aa15b021869f07c95018b94b06e071
Priyansh <Priyanshyadav1012@gmail.com>
2024-05-05 18:50:16 +0530

    DashBoard is in progress

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from '@/components/ui/HeaderBox';
 import React from 'react'
 
 const Home = () => {
+  const LoggedIn = {firstName : ' Priyansh '};
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header '>
+          <HeaderBox 
+            type='greeting'
+            title="Welcome"
+            user={LoggedIn ?.firstName || "Guest"}
+            subtext= "Access and Manage your account and Transaction"
+          />
+          <TotalBalanceBox 
+            accounts={[]} 
+            totalBanks={2}
+            totalCurrentBalance={12050.20}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
