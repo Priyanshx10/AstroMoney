020789a7d30a4fcc98e72d02a21a0cab10927ab1
Priyansh <Priyanshyadav1012@gmail.com>
2024-05-05 17:08:01 +0530

    SetUp

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets:["latin"],
+  weight:['700','400'],
+  variable:"--font-ibm-plex-serif"
+}) 
+
+export const metadata: Metadata = {
+  title: "AstroMoney",
+  description: "AstroMoney is the Banking Platform by Astro Nova Labs",
+  icons: '/icons/logo.svg'
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
