79c7809d2d5c2721fb0eb73652b3839c4dde0f04
Priyansh <Priyanshyadav1012@gmail.com>
2024-05-21 00:16:07 +0530

    3.0 Completed the signed in page .

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,18 +4,17 @@ declare type SearchParamProps = {
   params: { [key: string]: string };
   searchParams: { [key: string]: string | string[] | undefined };
 };
-
 // ========================================
 
 declare type SignUpParams = {
-  firstName: string;
-  lastName: string;
-  address1: string;
-  city: string;
-  state: string;
-  postalCode: string;
-  dateOfBirth: string;
-  ssn: string;
+  firstName?: string;
+  lastName?: string;
+  address1?: string;
+  city?: string;
+  state?: string;
+  postalCode?: string;
+  dateOfBirth?: string;
+  ssn?: string;
   email: string;
   password: string;
 };
